b03a50123bcdd144d891b9faf4aac2ae523f4458
brodeau <brodeau@gmail.com>
2022-09-20 11:57:59 +0200

    LB

--- 01_RGPS_selection.py
+++ 01_RGPS_selection.py
@@ -39,7 +39,7 @@ dt_tolr =    1*3600 ; # time interval aka tolerance `+-dt_tolr` to consider two 
 Nb_min_stream = 500 ; # minimum number of buoys to consider a stream a stream!
 
 
-idebug = 1 ; 
+idebug = 2 ; 
 
 idbg_subsmpl = 0 ; vIDdbg = [ 18 , 62 , 98, 1200, 2800, 3000, 8800, 10290, 16805 ] ; l_show_IDs_fig = True ; # Debug: pick a tiny
 
@@ -166,14 +166,18 @@ if __name__ == '__main__':
     #exit(0)
 
     #
-    Ns_max = 2000 # Max number of Stream
+    Ns_max = 2000 # Max number of Streams, guess!!! #fixme...
 
+    # Vectors along streams:
+    #VNB = nmp.zeros(  Ns_max     , dtype=int) - 999 ; # Number of valid buoys in each stream
+    VNB = [] ;  # Number of valid buoys in each stream
+    
     XIDs = nmp.zeros((Ns_max, Nb), dtype=int) - 999 ; # bad max size!! Stores the IDs used for a given stream...
     #XStr = nmp.zeros((Ns_max, Nb), dtype=int) - 999 ; # bad max size!! Stores the streams ....
     XNrc = nmp.zeros((Ns_max, Nb), dtype=int) - 999 ; # bad max size!! Stores the number of records
     XT1  = nmp.zeros((Ns_max, Nb), dtype=int) - 999 ; # bad max size!! Stores the number of records
     XT2  = nmp.zeros((Ns_max, Nb), dtype=int) - 999 ; # bad max size!! Stores the number of records
-
+    Xmsk = nmp.zeros((Ns_max, Nb), dtype=int)    
 
     xstreams = []
     rt_prev_stream = 1e12
@@ -188,7 +192,7 @@ if __name__ == '__main__':
         #
         if idebug>1: print("    => "+str(Np)+" buoys satisfy this!")
         #
-        Nb_stream = 0
+        Nbuoys_stream = 0
         
         if Np > Nb_min_stream and rt < rt_prev_stream+dt_buoy -dt_tolr:
             #
@@ -217,8 +221,9 @@ if __name__ == '__main__':
                 # We want at least 2 consecutive records for the buoy:
                 ntt = len(vt)
                 if ntt > 1:                    
-                    Nb_stream = Nb_stream + 1 ; # this is another valid buoy for this stream
-                    XIDs[istream,ib] = jid    ; # keeps memory of buoys that have been used!
+                    Nbuoys_stream = Nbuoys_stream + 1 ; # this is another valid buoy for this stream
+                    Xmsk[istream,ib] = 1      ; # valid point
+                    XIDs[istream,ib] = jid    ; # keeps memory of buoys that have been used!                    
                     #XStr[istream,ib] = istream; # keeps memory of stream
                     XNrc[istream,ib] = ntt    ; # keeps memory of stream
                     XT1[ istream,ib] = vt[0]  ; # keeps memory of
@@ -233,291 +238,46 @@ if __name__ == '__main__':
                         'ct0':     epoch2clock(vt[0]),
                         'ctN':     epoch2clock(vt[ntt-1]),
                         })
-                    #                    
+                    #
+
+            if Nbuoys_stream>1: print("  * we retained "+str(Nbuoys_stream)+" buoys in this stream...")
+            #VNB[istream] = Nbuoys_stream
+            VNB.append(Nbuoys_stream)
+            
         else:
             print("    => no buoys in this time range!")
 
-        if Nb_stream>1: print("  * we retained "+str(Nb_stream)+" buoys in this stream...")
             
-        if istream >= 1: break ; #DEBUG!
+            
+        if istream >= 2: break ; #DEBUG!
+    ### for jt in range(Nts)
 
-
-    #
-
-        
-    for js in range(2):
-        #print('\n *** Having a look at stream #'+str(js)+' => IDs =')
-        print('\n\n *** Having a look at stream #'+str(js)+' !')
-        #print(" xstreams['istream']", xstreams[0:10]['istream'])
-        #idx = nmp.where( xstreams['istream']==js )
-        #print( " idx = ", idx )
-
-
-
-
-        
-        exit(0)
-        
-        for jii in XIDs[js,:]:
-            if jii>0: print(jii)
-
-
-
-    exit(0)
-    
-    vt_earlst = nmp.zeros(Nb) ; ct_earlst = nmp.zeros(Nb, dtype='U20')
-    vt_latest = nmp.zeros(Nb) ; ct_latest = nmp.zeros(Nb, dtype='U20')
-
-    # For each buoy, getting date of earliest and latest snapshot
-    print("\n *** Scanning to study precise date range of this file...")
-
-    for jb in range(Nb):
-        if jb%1000 == 0: print("    .... "+str(jb)+" / "+str(Nb)+"...")
-
-        jid = vIDs[jb]
-
-        idx = nmp.where( vIDrgps0==jid )
-        vt  = vtime0[idx]
-        t1  = nmp.min(vt) ; ct1 = epoch2clock(t1)
-        t2  = nmp.max(vt) ; ct2 = epoch2clock(t2)
-
-        # Get rid of IDs that are seeded after rdt1:
-        if t1>rdt1:
-            print("   --- excluding buoy #"+str(jb)+" with ID: ",jid," (pops up after "+cdt1+"!)")
-            vmask[jb] = 0
-        if t2<=rdtI:
-            print("   --- excluding buoy #"+str(jb)+" with ID: ",jid," (vanishes before "+cdtI+"!)")
-            vmask[jb] = 0
-
-        if idebug>0 and Nb<=20: print("      * buoy #"+str(jb)+" (id="+str(jid)+": "+ct1+" ==> "+ct2)
-        vt_earlst[jb] = t1 ; ct_earlst[jb] = ct1 ;
-        vt_latest[jb] = t2 ; ct_latest[jb] = ct2 ;
-
-
-    NbR = nmp.sum(vmask)
-    print("\n *** Only "+str(NbR)+" buoys out of "+str(Nb)+" are remaining (time range)")
-
-    # Masking all destroyed buoys:
-    vIDs      =  nmp.ma.masked_where( vmask==0, vIDs      )
-    vt_earlst =  nmp.ma.masked_where( vmask==0, vt_earlst )
-    vt_latest =  nmp.ma.masked_where( vmask==0, vt_latest )
-
-    if idebug>1:
-        rt1  = nmp.min(vt_earlst)
-        rt2  = nmp.max(vt_latest)
-        rt1l = nmp.max(vt_earlst)
-        rt2l = nmp.min(vt_latest)
-        print("  --- absolute earliest date is: "+epoch2clock(rt1))
-        print("  --- absolute   latest date is: "+epoch2clock(rt2))
-        print("  --- latest   of the earliests dates is: "+epoch2clock(rt1l))
-        print("  --- earliest of the   latest  dates is: "+epoch2clock(rt2l),"\n")
-
-
-    # Dropping canceled (masked) buoys and updating number of valid buoys:
-    vIDs = nmp.ma.MaskedArray.compressed(vIDs)
-    Nb   = len(vIDs)
-    print("\n *** UPDATE: there are now "+str(Nb)+" buoys to follow!")
-
-
-    # Final arrays have 2 dimmensions Nb & Nt (buoy ID & time record)
-    xmsk = nmp.zeros((Nt,Nb), dtype=int)
-    xlon = nmp.zeros((Nt,Nb))
-    xlat = nmp.zeros((Nt,Nb))
-
-    xmsk[:,:] = 1
-
-    ic = -1
-    for jid in vIDs:
-        ic = ic + 1
-
-        #if vmask[ic] == 1:
-
-        idx  = nmp.where( vIDrgps0==jid )
-        vt   = vtime0[idx] ; # that's the time axis of this particular buoy [epoch time]
-        Np   = len(vt)    ; # Mind that Np varies from 1 buoy to another...
-
-        for jp in range(1,Np):
-            if vt[jp]<=vt[jp-1]:
-                print("ERROR: time not increasing","\n   ==> jp =",jp, " ==> ", vt[jp-1], " -- ", vt[jp])
-                exit(0)
-
-        Ntz = Nt
-
-        # Does this buoy survive beyond the fixed time range or not??? lilo
-        rtend = nmp.max(vt)
-        l_shorten = ( rtend < rdt2 )
-        if l_shorten:
-            if idebug>1: print("    * buoy with ID: "+str(jid)+" does not make it to "+cdt2, "\n      => dies at "+epoch2clock(rtend))
-            for Ntz in range(Nt):
-                rt = vTscan[Ntz]
-                if rt > rtend: break
-            if idebug>1: print("      ==> last index of vTscan to use is: "+str(Ntz-1)+" => "+epoch2clock(vTscan[Ntz-1]))
-
-        #f = interpolate.interp1d(vt, vlat) ; vintrp = f(vTscan)
-        #NUMPY: vintrp = nmp.interp(vTscan, vt, vlat) ; #, left=None, right=None, period=None)
-
-        if   interp_1d==0:
-            f = interpolate.interp1d(           vt, vlat0[idx])
-        elif interp_1d==1:
-            f = interpolate.Akima1DInterpolator(vt, vlat0[idx])
-        xlat[0:Ntz,ic] = f(vTscan[0:Ntz])
-
-        if   interp_1d==0:
-            f = interpolate.interp1d(           vt, vlon0[idx])
-        elif interp_1d==1:
-            f = interpolate.Akima1DInterpolator(vt, vlon0[idx])
-        xlon[0:Ntz,ic] = f(vTscan[0:Ntz])
-
-        if l_shorten: xmsk[Ntz:Nt,ic] = 0
-
-
-    #
-    xlat = nmp.ma.masked_where( xmsk==0, xlat )
-    xlon = nmp.ma.masked_where( xmsk==0, xlon )
+    # Masking arrays:
+    XIDs = nmp.ma.masked_where( Xmsk==0, XIDs )
 
     
-    # Removing doublons:
-    if l_drop_doublons:
-        from gonzag import Haversine
-
-        vid_mask = nmp.zeros(Nb, dtype=int)
-        vid_mask[:] = 1
-        
-        # Based on first time step:
-        jt   = 0
-        vlat = xlat[jt,:]
-        vlon = xlon[jt,:]
-        #
-        for jb in range(Nb):        
-            # All the following work should be done if present buoy has not been cancelled yet
-            if vid_mask[jb] == 1:
+    if idebug>0:
+        for js in range(len(VNB)):
+            vids = nmp.ma.MaskedArray.compressed( XIDs[js,:] ) ; # valid IDs for current stream: shrinked, getting rid of masked points
+            print('\n\n *** Having a look at stream #'+str(js)+' !')
+            print('     ===> has '+str(VNB[js])+' valid buoys!')
+            if idebug>1:
+                print('        => with following IDs:')
+                for jii in vids: print(jii,' ', end="")
+                print('')
+            #
+            # Show them on a map:
+            
                 
-                jid = vIDs[jb]
-                if idebug>0: print("\n *** 'TOO CLOSE' scanning: Buoy #"+str(jb)+"=> ID ="+str(jid))
+            #print(" xstreams['istream']", xstreams[0:10]['istream'])
+            #idx = nmp.where( xstreams['istream']==js )
+            #print( " idx = ", idx )
+
+
+
+
+
         
-                rlat = vlat[jb]
-                rlon = vlon[jb]
-        
-                if idebug>0: print("    ==> lat,lon = ",rlat,rlon)
-                
-                # Building array of distances (km) with all other buoy at this same time record:
-                vdist = Haversine( rlat, rlon, vlat, vlon )
-        
-                # Need to mask itself (distance = 0!)
-                if vdist[jb] != 0.:
-                    print(" PROBLEM: distance with yourself should be 0!")
-                vdist[jb] = 9999.
-        
-                if idebug>1:
-                    for rd in vdist:
-                        if rd<10.: print("   distance = ", rd)
-        
-                rdmin = nmp.min(vdist)
-                if idebug>0: print("    ==> closest other buoy is at "+str(round(rdmin,2))+" km !")
-                
-                if rdmin < rd_tol:
-                    # There is at least 1 buoy too close!
-                    #  => populating the buoys closer than `rd_tol` km:
-                    (vidx_close,) = nmp.where( vdist < rd_tol )
-        
-                    if idebug>0:
-                        print("    ==> list of buoys closer than "+str(rd_tol)+" km:")
-                        for jb in vidx_close:
-                            print(vIDs[ii])
-    
-                    vid_mask[(vidx_close,)] = 0
-
-        # Alright, all IDs spotted via vid_mask should be cancelled in the 2D fields, at all time steps
-        (idmsk,) = nmp.where(vid_mask==0)
-        xmsk[:,idmsk] = 0
-        vIDs = nmp.ma.masked_where( vid_mask==0, vIDs )
-        xlat = nmp.ma.masked_where( xmsk==0, xlat )
-        xlon = nmp.ma.masked_where( xmsk==0, xlon )
-        
-        Nok = nmp.sum(xmsk[0,:])
-        print("\n *** UPDATE: after 'too close' canceling, there are "+str(Nok)+" buoys left at t0!")
-
-        # Need to compress arrays, didn't find an elegant way to do it so here is the following abomination:
-        xmsk2 = nmp.zeros((Nt,Nok), dtype=int)
-        xlon2 = nmp.zeros((Nt,Nok))
-        xlat2 = nmp.zeros((Nt,Nok))
-        ic = 0
-        for jb in range(Nb):
-            if xmsk[0,jb] == 1:
-                xmsk2[:,ic] = xmsk[:,jb]
-                xlon2[:,ic] = xlon[:,jb]
-                xlat2[:,ic] = xlat[:,jb]
-                ic = ic+1
-        vIDs = nmp.ma.MaskedArray.compressed(vIDs)
-        Nb = Nok
-        #print("Shape of vIDs, xlat2, xlon2, xmsk2 =",vIDs.shape,xlat2.shape,xlon2.shape,xmsk2.shape)
-        del xmsk, xlat, xlon
-        xmsk = xmsk2
-        xlat = xlat2
-        xlon = xlon2
-        del xmsk2, xlat2, xlon2
-        #print("Shape of vIDs, xlat, xlon, xmsk =",vIDs.shape,xlat.shape,xlon.shape,xmsk.shape)
-        #exit(0)
-        
-    #endif l_drop_doublons
-
-    # convert to meters in neXtSIM projection
-    from cartopy.crs import NorthPolarStereo
-    srs_nextsim = NorthPolarStereo(central_longitude=-45, true_scale_latitude=60)
-    srs_rgps    = NorthPolarStereo(central_longitude=-45, true_scale_latitude=70)
-    xpos,ypos,_ = srs_nextsim.transform_points(srs_rgps, xlon, xlat).T * 1000.
-
-    xp = xpos.T
-    yp = ypos.T
-    del xpos,ypos
-
-    yp   = nmp.ma.masked_where( xmsk==0, yp )
-    xp   = nmp.ma.masked_where( xmsk==0, xp )
-    xlat = nmp.ma.masked_where( xmsk==0, xlat )
-    xlon = nmp.ma.masked_where( xmsk==0, xlon )
-
-    
-    
-    
-    # GENERATION OF COMPREHENSIVE NETCDF FILE
-    #########################################
 
 
-    print("\n *** About to generate file: "+cf_out+" ...")
 
-    f_out = Dataset(cf_out, 'w', format='NETCDF4')
-
-    cd_time = 'time'
-    cd_buoy = 'id_buoy'
-
-    #Dimensions:
-    f_out.createDimension(cd_time, None)
-    f_out.createDimension(cd_buoy, Nb  )
-
-    v_time  = f_out.createVariable(cd_time, 'i4',(cd_time,))
-    v_id    = f_out.createVariable(cd_buoy, 'i4',(cd_buoy,))
-    v_latb  = f_out.createVariable('latitude' , 'f4',(cd_time,cd_buoy,), zlib=True, complevel=9)
-    v_lonb  = f_out.createVariable('longitude', 'f4',(cd_time,cd_buoy,), zlib=True, complevel=9)
-    v_y     = f_out.createVariable('y_pos' ,    'f4',(cd_time,cd_buoy,), zlib=True, complevel=9)
-    v_x     = f_out.createVariable('x_pos',     'f4',(cd_time,cd_buoy,), zlib=True, complevel=9)
-
-    v_time.units = ctunits
-    v_id.units   = "ID of buoy"
-    v_latb.units = "degrees north"
-    v_lonb.units = "degrees south"
-    v_y.units    = "m"
-    v_x.units    = "m"
-
-    v_id[:] = vIDs[:]
-
-    for jt in range(Nt):
-        v_time[jt]   = vTscan[jt]
-        v_latb[jt,:] =  xlat[jt,:]
-        v_lonb[jt,:] =  xlon[jt,:]
-        v_y[jt,:]    =  yp[jt,:]
-        v_x[jt,:]    =  xp[jt,:]
-
-    f_out.About  = 'RGPS sea-ice drift data (Kwok, 1998)'
-    f_out.Author = 'Generated with `'+path.basename(argv[0])+'` (L. Brodeau, 2022)'
-    f_out.close()
-    print("      ===> "+cf_out+" saved!")
