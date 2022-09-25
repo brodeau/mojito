47a96571bd8caabcf53facc9143a3f7d388d09e4
brodeau <brodeau@gmail.com>
2022-09-25 12:51:15 +0200

    LB

--- test_mesh_generation.py
+++ test_mesh_generation.py
@@ -5,9 +5,11 @@
 
 #from sys import argv, exit
 #from os import path
-#import numpy as nmp
+import numpy as nmp
 
-import pandas as pd
+from scipy.spatial import Delaunay
+
+#import pandas as pd
 
 
 xcapitals = []
@@ -19,11 +21,45 @@ xcapitals.append({"city":"Athen"  , "lat":37.984149, "lon":23.727984})
 xcapitals.append({"city":"Belgrad", "lat":44.817813, "lon":20.456897})
 xcapitals.append({"city":"Berlin" , "lat":52.517037, "lon":13.388860})
 xcapitals.append({"city":"Bern"   , "lat":46.948271, "lon":7.451451 })
+# 51.510433, -0.129711
+xcapitals.append({"city":"London" , "lat":51.510433, "lon":-0.129711 })
 
 
 Nbc = len(xcapitals)
 
 print('\n We have '+str(Nbc)+' cities!')
 
-print( xcapitals[2]["city"] )
+print( xcapitals[0]["city"] )
 
+yk, xk = nmp.zeros(Nbc), nmp.zeros(Nbc)
+
+for jc in range(Nbc):
+    yk[jc], xk[jc] =  xcapitals[jc]["lat"], xcapitals[jc]["lon"]
+
+#print(yk) ; print('') ; print(xk)
+
+X1  = nmp.vstack((xk,yk)).T
+tri = Delaunay(X1)
+
+
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+#--- grafics -------
+figX = 25; figY = 18
+fig1 = plt.figure(figsize=(figX, figY), facecolor='white')
+
+myProjection = ccrs.PlateCarree()
+ax = plt.axes(projection=myProjection)
+ax.stock_img()
+ax.set_extent([-25, 40, 35, 65], crs=myProjection)
+
+plt.triplot(X1[:,0], X1[:,1], tri.simplices.copy(), color='r', linestyle='-',lw=2)
+plt.plot(X1[:,0], X1[:,1], 's', color='w')
+
+plt.scatter(xk,yk,s=1000,c='w')
+#for i, txt in enumerate(city):
+#    ax.annotate(txt, (X1[i,0], X1[i,1]), color='k', fontweight='bold')
+
+plt.savefig('Europe_A.png')
+plt.show()
