a45519459343868d2793c99219e4b6faa2889ee1
brodeau <brodeau@gmail.com>
2022-09-28 17:06:25 +0200

    LB

---
+++ test_mesh_generation_REAL.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+##################################################################
+
+from sys import argv, exit
+#from os import path
+import numpy as nmp
+
+from scipy.spatial import Delaunay
+
+import lbrgps   as lbr
+
+idebug=2
+
+
+cf_in = 'npz/SELECTION_buoys_RGPS_stream001_1997-01-04.npz'
+
+data = nmp.load(cf_in)
+
+it   = data['itime']
+vlon = data['vlon']
+vlat = data['vlat']
+
+print(it)
+
+
+
+exit(0)
+
+
+NbP = len(xcities) ; # number of points
+
+print('\n *** We have '+str(NbP)+' cities!')
+
+
+# Conversion of dictionary to Numpy arrays:
+vIDs  = nmp.array([ xcities[jc]["ID"]                      for jc in range(NbP) ], dtype=int  )
+Xcoor = nmp.array([[xcities[jc]["lon"],xcities[jc]["lat"]] for jc in range(NbP) ]             )
+vnam  = nmp.array([ xcities[jc]["city"]                    for jc in range(NbP) ], dtype='U32')
+
+if idebug>0:
+    for jc in range(NbP):
+        print(' * '+vnam[jc]+': ID='+str(vIDs[jc])+', lat='+str(round(Xcoor[jc,1],2))+', lon='+str(round(Xcoor[jc,0],2)))
+    print('')
+
+
+# Generating triangular meshes out of the cloud of points:
+TRI = Delaunay(Xcoor)
+
+xTriangles = TRI.simplices.copy() ; # shape = (Nbt,3) A simplex of 2nd order is a triangle! *_*
+
+(NbT,_) = nmp.shape(xTriangles) ; # NbT => number of triangles
+
+xNeighbors = TRI.neighbors.copy() ;  # shape = (Nbt,3)
+
+print('\n *** We have '+str(NbT)+' triangles!')
+
+#print('\n',nmp.shape(TRI.points))
+#exit(0)
+
+
+
+if idebug>1:
+    for jx in range(NbT):
+        vpl = xTriangles[jx,:] ; # 3 point indices forming the triangle
+        print(' Triangle #'+str(jx)+': ', vpl[:],'aka "'+vnam[vpl[0]]+' - '+vnam[vpl[1]]+' - '+vnam[vpl[2]]+'"')
+        print('    => neighbor triangles are:',xNeighbors[jx,:],'\n')
+
+if idebug>1:
+    # In which simplex (aka triangle) is Grenoble:
+    vlocate = nmp.array([(x_gre,y_gre)])
+    kv_gre  = TRI.find_simplex(vlocate)
+    jx      = kv_gre[0]
+    vpl     = xTriangles[jx,:]
+    print('\n *** Grenoble is located in triangle #'+str(jx)+':', vpl[:],'aka "'+vnam[vpl[0]]+' - '+vnam[vpl[1]]+' - '+vnam[vpl[2]]+'"\n')
+
+
+# Show triangles on a map:
+kk = lbr.ShowTMeshMap( Xcoor[:,0], Xcoor[:,1], xTriangles, cfig="01_Mesh_Map_TRIangles_Europe.png", pnames=vnam )
+
+# Merge triangles into quadrangles:
+xQuads = lbr.Triangles2Quads( xTriangles, xNeighbors, Xcoor, vnam,  iverbose=idebug )
+
+(NbQ,_) = nmp.shape(xQuads)
+print('\n *** We have '+str(NbQ)+' quadrangles!')
+
+# Show quadrangles on a map:
+kk = lbr.ShowQMeshMap( Xcoor[:,0], Xcoor[:,1], xQuads, cfig="02_Mesh_Map_Quadrangles_Europe.png", pnames=vnam, TriMesh=xTriangles )
+
