de034a39da6eca2b94ea8b81c87afc47a546a017
brodeau <brodeau@gmail.com>
2022-10-11 16:03:38 +0200

    LB

--- 03_deformations.py
+++ 03_deformations.py
@@ -52,46 +52,56 @@ nQ = len(vnm) ; # also = len(vidx*)
 print('\n *** There are '+str(nQ)+' Quads common to the 2 records!')
 
 # Coordinates of the 4 points of quadrangles:
-zXY1 , zXY2 = np.zeros((nQ,4,2)) - 999. , np.zeros((nQ,4,2)) - 999.
-zXY1[:,:,:] = QUA1.MeshPointXY[vidx1,:,:]
-zXY2[:,:,:] = QUA2.MeshPointXY[vidx2,:,:]
+#zXY1 , zXY2 = np.zeros((nQ,4,2)) - 999. , np.zeros((nQ,4,2)) - 999.
+zXY1 = QUA1.MeshPointXY[vidx1,:,:].copy()
+zXY2 = QUA2.MeshPointXY[vidx2,:,:].copy()
+# Same, but at center of time interval:
+zX , zY = np.zeros((nQ,4)) , np.zeros((nQ,4))
+zX = 0.5*( zXY1[:,:,0] + zXY2[:,:,0] )
+zY = 0.5*( zXY1[:,:,1] + zXY2[:,:,1] )
 
 # Area of quadrangles:
-zA1 , zA2 = np.zeros(nQ) - 999. , np.zeros(nQ) - 999.
-zA1[:] = QUA1.area()[vidx1]
-zA2[:] = QUA2.area()[vidx2]
-
+#zA1 , zA2 = np.zeros(nQ) - 999. , np.zeros(nQ) - 999.
+#zA1[:] = QUA1.area()[vidx1]
+#zA2[:] = QUA2.area()[vidx2]
+# Area of quadrangles at center of time interval:
+zA = np.zeros(nQ) - 999.
+zA[:] = 0.5*( QUA1.area()[vidx1] + QUA2.area()[vidx2] )
 
 # Velocities at center of time interval:
 zU = 1000. * np.array( [ zXY2[:,k,0] - zXY1[:,k,0] for k in range(4) ] ).T / dt ; # 1000 because X,Y in km !!!
 zV = 1000. * np.array( [ zXY2[:,k,1] - zXY1[:,k,1] for k in range(4) ] ).T / dt ; # 1000 because X,Y in km !!!
+#zuu , zvv = np.zeros((nQ,4)) - 999. , np.zeros((nQ,4)) - 999.
+#for jQ in range(nQ):
+#    zuu[jQ,:] = 1000. * np.array( [ zXY2[jQ,k,0] - zXY1[jQ,k,0] for k in range(4) ] ) / dt
+#    zvv[jQ,:] = 1000. * np.array( [ zXY2[jQ,k,1] - zXY1[jQ,k,1] for k in range(4) ] ) / dt
+del zXY1, zXY2
+
+
+if idebug>0:
+    for jQ in range(0,nQ,100):
+        print('  areas =',np.round(zA[jQ],3),'km^2, U =',np.round(zU[jQ,:],5),'m/s, V =',np.round(zV[jQ,:],5),'m/s')
+
+
+# Partial derivatives:
+zdUdx , zdUdy = np.zeros(nQ) , np.zeros(nQ)
+zdVdx , zdVdy = np.zeros(nQ) , np.zeros(nQ)
 
 for jQ in range(nQ):
-    print('  areas =',np.round(zA1[jQ],3),np.round(zA2[jQ],3),' U =',np.round(zU[jQ,:],5),' V =',np.round(zV[jQ,:],5))
+    zdUdx[jQ] =  np.sum( np.array([ (zU[jQ,(k+1)%4] + zU[jQ,k])*(zY[jQ,(k+1)%4] - zY[jQ,k]) for k in range(4) ]) ) / (2*zA[jQ])
+    zdUdy[jQ] = -np.sum( np.array([ (zU[jQ,(k+1)%4] + zU[jQ,k])*(zX[jQ,(k+1)%4] - zX[jQ,k]) for k in range(4) ]) ) / (2*zA[jQ])
+    zdVdx[jQ] =  np.sum( np.array([ (zV[jQ,(k+1)%4] + zV[jQ,k])*(zY[jQ,(k+1)%4] - zY[jQ,k]) for k in range(4) ]) ) / (2*zA[jQ])
+    zdVdy[jQ] = -np.sum( np.array([ (zV[jQ,(k+1)%4] + zV[jQ,k])*(zX[jQ,(k+1)%4] - zX[jQ,k]) for k in range(4) ]) ) / (2*zA[jQ])
 
-
-exit(0)
-
-
-
-
-
-
-
-
-
-
-
-
-# Getting the angles:
-zAngles  = QUA1.angles()
-zLengths =  QUA1.lengths()
-
-for jQ in range(nQ):
-    print('\n *** Quad #'+str(jQ)+' => angles =',zAngles[jQ,:],' => lengths =',zLengths[jQ,:])
+if idebug>0:
+    for jQ in range(0,nQ,100):
+        print('  dU/dx =',zdUdx[jQ],'1/s, dU/dy =',zdUdy[jQ],'1/s')
+        print('  dV/dx =',zdVdx[jQ],'1/s, dV/dy =',zdVdy[jQ],'1/s\n')
+    
 
 
 
 
+        
 # Show the quads with only the points that define them:
 #kk = lbr.ShowTQMesh( QUA1.PointXY[:,0], QUA1.PointXY[:,1], cfig=cf_fig, QuadMesh=QUA1.MeshPointIDs, lProj=False, zoom=izoom )
