4013529ffc9e75ef4dbe7cf54e3d30b05be14249
brodeau <brodeau@gmail.com>
2022-10-11 10:06:36 +0200

    LB

---
+++ 03_deformations.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+##################################################################
+
+from sys import argv, exit
+from os import path
+import numpy as np
+from re import split
+
+from scipy.spatial import Delaunay
+
+from climporn import epoch2clock
+import lbrgps   as lbr
+
+idebug=1
+
+if not len(argv) in [3]:
+    print('Usage: '+argv[0]+' <file_Q_mesh_N1.npz> <file_Q_mesh_N2.npz>')
+    exit(0)
+
+cf_Q1 = argv[1]
+cf_Q2 = argv[2]
+
+
+print('')
+
+#cf_fig = str.replace( path.basename(cf_Q1), '.npz', '.png' )
+
+# Reading the quad meshes in the npz files:
+QUA1 = lbr.LoadClassPolygon( cf_Q1, ctype='Q' )
+QUA2 = lbr.LoadClassPolygon( cf_Q2, ctype='Q' )
+
+
+nP = QUA1.nP
+nQ = QUA1.nQ
+
+print('\n nP:',QUA1.nP,QUA2.nP)
+print('\n nQ:',QUA1.nQ,QUA2.nQ)
+exit(0)
+
+# Getting the angles:
+zAngles  = QUA1.angles()
+zLengths =  QUA1.lengths()
+
+for jQ in range(nQ):
+    print('\n *** Quad #'+str(jQ)+' => angles =',zAngles[jQ,:],' => lengths =',zLengths[jQ,:])
+
+
+
+
+# Show the quads with only the points that define them:
+#kk = lbr.ShowTQMesh( QUA1.PointXY[:,0], QUA1.PointXY[:,1], cfig=cf_fig, QuadMesh=QUA1.MeshPointIDs, lProj=False, zoom=izoom )
