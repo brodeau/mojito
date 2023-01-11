cbadb2e67e46219568241325694cee7e254a237c
brodeau <brodeau@gmail.com>
2023-01-11 17:07:19 +0100

    LB

--- TEST_rgps/do2.sh
+++ TEST_rgps/do2.sh
@@ -2,21 +2,23 @@
 
 . ./conf.bash
 
-EXE="${MOJITO_DIR}/rgps/03_generate_quad_mesh.py"
+#EXE="${MOJITO_DIR}/rgps/03_generate_quad_mesh.py"
+EXE="${MOJITO_DIR}/rgps/03_generate_quad_mesh_multi.py"
 
 #list=`\ls npz/SELECTION_buoys_RGPS_stream*.npz`
-list=`\ls npz/SELECTION_buoys_RGPS_stream000_*.npz`
+#list=`\ls npz/SELECTION_buoys_RGPS_stream000_*.npz`
 
-for ff in ${list}; do
-    
-    for dd in ${LIST_RES}; do
-    
-        CMD="${EXE} ${ff} ${dd}"
-        echo; echo; echo "  ==> ${CMD}"; echo
-        ${CMD}
-        echo
-        
-    done
+pref="npz/SELECTION_buoys_RGPS_stream000"
+
+#for ff in ${list}; do
+
+for dd in ${LIST_RES}; do
+
+    CMD="${EXE} ${pref} ${dd}"
+    echo; echo; echo "  ==> ${CMD}"; echo
+    ${CMD}
+    echo
 
 done
 
+#done
--- rgps/03_generate_quad_mesh_multi.py
+++ rgps/03_generate_quad_mesh_multi.py
@@ -7,6 +7,7 @@
 
 from sys import argv, exit
 from os import path
+from glob import glob
 import numpy as np
 from re import split
 from math import copysign
@@ -38,9 +39,9 @@ rzoom_fig = 5
 if __name__ == '__main__':
 
     if not len(argv) in [2,3]:
-        print('Usage: '+argv[0]+' <list of SELECTION_* files (separated b)> (<min_pt_spacing_km>)')
+        print('Usage: '+argv[0]+' <SELECTION_streamXXX> (<min_pt_spacing_km>)')
         exit(0)
-    cf_npz = argv[1]    
+    cpref_npz = argv[1]    
     l_force_min_scale = ( len(argv) == 3 and argv[2] != '0' )
     
     rd_spacing = rd_nom_data
@@ -51,15 +52,20 @@ if __name__ == '__main__':
     cL_spacing = '%2.2i'%int(round(rd_spacing,0))
 
 
-    print('\n\n**********************************************************************')
-    print(' *** Ok! Scale we are going to use is: `rd_spacing` = ',str(rd_spacing)+'km')
+    # List of files to use from `cpref_npz`: lilo
+    list_npz = np.sort( glob(cpref_npz+'*') )
+
+    NbF = len(list_npz)
+    if NbF < 2:
+        print('ERROR: we need at least 2 npz files!!!')
+        exit(0)
     
-    # Strings for names of output files:
-    cfroot = str.replace( split('.npz',path.basename(cf_npz))[0] , 'SELECTION_buoys_RGPS_','' )
-    if l_force_min_scale:
-        cfroot += '_'+cL_spacing+'km_Sampled'
-    else:
-        cfroot += '_'+cL_spacing+'km_NoSample'
+
+        
+    print('\n\n**********************************************************************')
+    print(' *** The '+str(NbF)+' files to use:')
+    for ff in list_npz: print('       * '+ff)
+    print('\n *** Scale we are going to use is: `rd_spacing` = ',str(rd_spacing)+'km')
     
     rA_nom = rd_spacing*rd_spacing ; # expected nominal area of the quadrangles [km^2]
     rf1 , rf2 = 1.-rcAtol , 1.+rcAtol
@@ -70,172 +76,199 @@ if __name__ == '__main__':
     print('    ==> '+str(rA_nom)+'km^2 ('+str(rQarea_min)+'km^2 < A < '+str(rQarea_max)+'km^2)')
     print('**********************************************************************\n')
 
+    # Loop along the `NbF` npz files!
+    #  - Delaunay triangulation should only be done with first file
+    #  - For following files we track the same QUADs (as built from first file) and update the coordinates of the points
+
+    for jf in range(NbF):
+
+        cf_npz = list_npz[jf]
+        
+        print('\n ### File # '+str(jf+1)+' !\n','     => '+cf_npz)
+
+
+        
+        # Strings for names of output files:
+        cfroot = str.replace( split('.npz',path.basename(cf_npz))[0] , 'SELECTION_buoys_RGPS_','' )
+        if l_force_min_scale:
+            cfroot += '_'+cL_spacing+'km_Sampled'
+        else:
+            cfroot += '_'+cL_spacing+'km_NoSample'
+
+        print('    ==> cfroot =', cfroot)
+       
+        l_someQuads = True
+        cf_npzT = './npz/T-mesh_'+cfroot+'.npz'
+        cf_npzQ = './npz/Q-mesh_'+cfroot+'.npz'
+        exit(0)
+        if not path.exists(cf_npzT) or not path.exists(cf_npzQ) or idebug>0:
     
-    l_someQuads = True
-    cf_npzT = './npz/T-mesh_'+cfroot+'.npz'
-    cf_npzQ = './npz/Q-mesh_'+cfroot+'.npz'
-
-    if not path.exists(cf_npzT) or not path.exists(cf_npzQ) or idebug>0:
-
-        print('\n *** We are going to build triangle and quad meshes!')
-        print('     => desired scale for quadrangles = '+str(int(rd_spacing))+'km')
-        print('     => area range for a quadrangle to qualify: '+str(int(rQarea_min))+'km^2 < A < '+str(int(rQarea_max))+'km^2')
-        
-        print('\n *** Reading into '+cf_npz+' !!!')
-        with np.load(cf_npz) as data:
-            it     = data['itime']
-            cdate  = str( data['date'] )
-            Nbuoys = data['Npoints']
-            vids   = data['vids']
-            vx = data['vx']
-            vy = data['vy']
-            if len(vids) != len(vx) or len(vids) != len(vy): print('ERROR Y11!') ; exit(0)
-
-        NbP = len(vids) ; # number of points
-        if NbP != Nbuoys: print('ERROR: NbP != Nbuoys !'); exit(0)
-
-        if len(vx)!=NbP or len(vy)!=NbP:      print('ERROR Y13!') ; exit(0)
-        if len(vids) != len(np.unique(vids)): print('ERROR Y14!') ; exit(0)
-
-        print('\n *** Stream at '+epoch2clock(it)+' => '+str(NbP)+' points!')
-
-        vIDs  = np.array( vids )
-        del vids
-
-
-        # Name for each point:
-        vPnam = np.array( [ str(i) for i in vIDs ], dtype='U32' )
-
-
-        xCoor = np.array( [ [vx[i],vy[i]] for i in range(NbP) ] ) ; # original x,y cartesian cordinates of the RGPS data!
-        #                                                           # => Polar Stereographic proj., lon_0=-45, lat_ts=70        
-        if idebug>2:
-            for jc in range(NbP):
-                print(' * Name: "'+vPnam[jc]+'": ID='+str(vIDs[jc])
-                      +', x ='+str(round(xCoor[jc,0],2))+', y ='+str(round(xCoor[jc,1],2)))
-            print('')
-
-
-        
-        l_happy = False
-        itt     = 0
-        rfcorr  = 1.
-        rdev    = 1.
-        
-        while not l_happy:
-            itt = itt + 1
+            print('\n *** We are going to build triangle and quad meshes!')
+            print('     => desired scale for quadrangles = '+str(int(rd_spacing))+'km')
+            print('     => area range for a quadrangle to qualify: '+str(int(rQarea_min))+'km^2 < A < '+str(int(rQarea_max))+'km^2')
             
-            # Just prior to Delaunay we may have to sub-sample in space the cloud of point
-            if l_force_min_scale:
-                    
-                rd_ss = rfcorr * rd_spacing ; # Correct `rd_spacing` to get closer to requested radius (based on QUADs to be generated)
-
-                print('\n *** Applying spatial sub-sampling! Threshold radius: '+str(round(rd_ss,2))+'km')                
-                NbPss, zCoor, zIDs, zPnam = mjt.SubSampCloud( rd_ss, xCoor, vIDs,  pNames=vPnam )
+            print('\n *** Reading into '+cf_npz+' !!!')
+            with np.load(cf_npz) as data:
+                it     = data['itime']
+                cdate  = str( data['date'] )
+                Nbuoys = data['Npoints']
+                vids   = data['vids']
+                vx = data['vx']
+                vy = data['vy']
+                if len(vids) != len(vx) or len(vids) != len(vy): print('ERROR Y11!') ; exit(0)
+    
+            NbP = len(vids) ; # number of points
+            if NbP != Nbuoys: print('ERROR: NbP != Nbuoys !'); exit(0)
+    
+            if len(vx)!=NbP or len(vy)!=NbP:      print('ERROR Y13!') ; exit(0)
+            if len(vids) != len(np.unique(vids)): print('ERROR Y14!') ; exit(0)
+    
+            print('\n *** Stream at '+epoch2clock(it)+' => '+str(NbP)+' points!')
+    
+            vIDs  = np.array( vids )
+            del vids
+    
+    
+            # Name for each point:
+            vPnam = np.array( [ str(i) for i in vIDs ], dtype='U32' )
+    
+    
+            xCoor = np.array( [ [vx[i],vy[i]] for i in range(NbP) ] ) ; # original x,y cartesian cordinates of the RGPS data!
+            #                                                           # => Polar Stereographic proj., lon_0=-45, lat_ts=70        
+            if idebug>2:
+                for jc in range(NbP):
+                    print(' * Name: "'+vPnam[jc]+'": ID='+str(vIDs[jc])
+                          +', x ='+str(round(xCoor[jc,0],2))+', y ='+str(round(xCoor[jc,1],2)))
+                print('')
+    
+    
+            
+            l_happy = False
+            itt     = 0
+            rfcorr  = 1.
+            rdev    = 1.
+            
+            while not l_happy:
+                itt = itt + 1
                 
-                if idebug>0:
-                    # Shows the cloud of buoys (with buoys' IDs) on the Cartesian plane (km)
-                    # After and before subsampling
-                    zrx = [ np.min(xCoor[:,0])-50. , np.max(xCoor[:,0])+50. ]
-                    zry = [ np.min(xCoor[:,1])-50. , np.max(xCoor[:,1])+50. ]
-                    #
-                    kk = mjt.ShowTQMesh( xCoor[:,0], xCoor[:,1], cfig='./figs/00_Original_'+cfroot+'.png',
-                                         ppntIDs=vIDs, lGeoCoor=False, zoom=rzoom_fig, rangeX=zrx, rangeY=zry )
-                    kk = mjt.ShowTQMesh( zCoor[:,0], zCoor[:,1], cfig='./figs/00_SubSamp_'+cfroot+'.png',
-                                         ppntIDs=zIDs, lGeoCoor=False, zoom=rzoom_fig, rangeX=zrx, rangeY=zry )
-                #lilo
-            else:
-                NbPss = NbP
-                zCoor = xCoor
-                zIDs  = vIDs
-                zPnam = vPnam
-                
-    
-            # Generating triangular meshes out of the cloud of points:
-            TRI = Delaunay(zCoor)
-    
-            xTpnts = TRI.simplices.copy() ; # shape = (Nbt,3) A simplex of 2nd order is a triangle! *_*
-    
-            (NbT,_) = np.shape(xTpnts) ; # NbT => number of triangles
-    
-            xNeighborIDs = TRI.neighbors.copy() ;  # shape = (Nbt,3)
-    
-            print('\n *** We have '+str(NbT)+' triangles!')
-    
-            # Conversion to the `Triangle` class:
-            TRIAS = mjt.Triangle( zCoor, xTpnts, xNeighborIDs, zIDs, zPnam ) ; #lolo
-    
-            del xTpnts, xNeighborIDs, TRI
-            
-            
-            # Merge triangles into quadrangles:
-            xQcoor, vPQids, xQpnts, vQnam = mjt.Tri2Quad( TRIAS, iverbose=idebug, anglRtri=(rTang_min,rTang_max),
-                                                          ratioD=rdRatio_max, anglR=(rQang_min,rQang_max),
-                                                          areaR=(rQarea_min,rQarea_max) )
-            l_someQuads = (len(xQpnts)>0)
-            #if len(xQpnts)<=0: exit(0)
-
-            if l_someQuads:
-                (NbQ,_) = np.shape(xQpnts)
-                print('\n *** We have '+str(NbQ)+' quadrangles!')
-    
-                # Conversion to the `Quadrangle` class (+ we change IDs from triangle world [0:nT] to that of quad world [0:nQ]):
-                QUADS = mjt.Quadrangle( xQcoor, xQpnts, vPQids, vQnam, date=cdate )
-
-                del xQpnts, xQcoor, zCoor
-            
-                zsides = QUADS.lengths()
-                zareas = QUADS.area()
-                rl_average_side = np.mean(zsides)
-                rl_average_scal = np.mean( np.sqrt(zareas) )
-                rl_average_area = np.mean(zareas)
-                print('    ==> average scale (sqrt[A]) is '+str(round(rl_average_scal,3))+' km')
-                print('    ==> average side length is '+str(round(rl_average_side,3))+' km')
-                print('    ==> average area is '+str(round(rl_average_area,1))+' km^2')
-                del zareas, zsides
-    
+                # Just prior to Delaunay we may have to sub-sample in space the cloud of point
                 if l_force_min_scale:
-                    rdev_old = rdev
-                    #rdev = rl_average_side - rd_spacing
-                    rdev = rl_average_scal - rd_spacing
-                    l_happy = ( abs(rdev) < rtol ) ; # average quadrangle side is close to expected nominal scale
-
-                    if not l_happy and itt==5:
-                        # We give up after 5 itterations!
-                        print(' +++++ WE GIVE UP !!! ++++++')
-                        l_happy = True
+                        
+                    rd_ss = rfcorr * rd_spacing ; # Correct `rd_spacing` to get closer to requested radius (based on QUADs to be generated)
+    
+                    print('\n *** Applying spatial sub-sampling! Threshold radius: '+str(round(rd_ss,2))+'km')                
+                    NbPss, zCoor, zIDs, zPnam = mjt.SubSampCloud( rd_ss, xCoor, vIDs,  pNames=vPnam )
                     
-                    if not l_happy:
-                        # Linear fit of actual correction as a function of `rd_spacing`
-                        rfc = 0.008*rd_spacing + 0.56                
-                        if itt==1: ralpha = (1.-rfc) / rdev ; # equivalent to a correction of `rfc`
-                        if itt>1 and copysign(1,rdev) == -copysign(1,rdev_old):
-                            ralpha = ralpha/1.3 ; # change of sign of deviation => we decrease alpha!
-                        # will go for a next round with a correction factor becoming increasingly smaller than 1:
-                        #rfcorr = min( max(0.6 , rfcorr - ralpha * rdev ) , 0.95 )
-                        rfcorr = min( max(0.5 , rfcorr - ralpha * rdev ) , 0.95 )
-                        print(' +++++ NEW itteration with: ralpha, rfcorr = ',ralpha, rfcorr)
-                    #
+                    if idebug>0:
+                        # Shows the cloud of buoys (with buoys' IDs) on the Cartesian plane (km)
+                        # After and before subsampling
+                        zrx = [ np.min(xCoor[:,0])-50. , np.max(xCoor[:,0])+50. ]
+                        zry = [ np.min(xCoor[:,1])-50. , np.max(xCoor[:,1])+50. ]
+                        #
+                        kk = mjt.ShowTQMesh( xCoor[:,0], xCoor[:,1], cfig='./figs/00_Original_'+cfroot+'.png',
+                                             ppntIDs=vIDs, lGeoCoor=False, zoom=rzoom_fig, rangeX=zrx, rangeY=zry )
+                        kk = mjt.ShowTQMesh( zCoor[:,0], zCoor[:,1], cfig='./figs/00_SubSamp_'+cfroot+'.png',
+                                             ppntIDs=zIDs, lGeoCoor=False, zoom=rzoom_fig, rangeX=zrx, rangeY=zry )
+                    #lilo
                 else:
-                    l_happy = True ; # we work with the data's nominal scale so we just go on here...
-            else:
-                l_happy = True ; # we coud not build any QUAD so we move on anyways...
+                    NbPss = NbP
+                    zCoor = xCoor
+                    zIDs  = vIDs
+                    zPnam = vPnam
+                    
+        
+                # Generating triangular meshes out of the cloud of points:
+                TRI = Delaunay(zCoor)
+        
+                xTpnts = TRI.simplices.copy() ; # shape = (Nbt,3) A simplex of 2nd order is a triangle! *_*
+        
+                (NbT,_) = np.shape(xTpnts) ; # NbT => number of triangles
+        
+                xNeighborIDs = TRI.neighbors.copy() ;  # shape = (Nbt,3)
+        
+                print('\n *** We have '+str(NbT)+' triangles!')
+        
+                # Conversion to the `Triangle` class:
+                TRIAS = mjt.Triangle( zCoor, xTpnts, xNeighborIDs, zIDs, zPnam ) ; #lolo
+        
+                del xTpnts, xNeighborIDs, TRI
                 
-        ###################
-        # #while not l_happy
+                
+                # Merge triangles into quadrangles:
+                xQcoor, vPQids, xQpnts, vQnam = mjt.Tri2Quad( TRIAS, iverbose=idebug, anglRtri=(rTang_min,rTang_max),
+                                                              ratioD=rdRatio_max, anglR=(rQang_min,rQang_max),
+                                                              areaR=(rQarea_min,rQarea_max) )
+                l_someQuads = (len(xQpnts)>0)
+                #if len(xQpnts)<=0: exit(0)
+    
+                if l_someQuads:
+                    (NbQ,_) = np.shape(xQpnts)
+                    print('\n *** We have '+str(NbQ)+' quadrangles!')
+        
+                    # Conversion to the `Quadrangle` class (+ we change IDs from triangle world [0:nT] to that of quad world [0:nQ]):
+                    QUADS = mjt.Quadrangle( xQcoor, xQpnts, vPQids, vQnam, date=cdate )
+    
+                    del xQpnts, xQcoor, zCoor
+                
+                    zsides = QUADS.lengths()
+                    zareas = QUADS.area()
+                    rl_average_side = np.mean(zsides)
+                    rl_average_scal = np.mean( np.sqrt(zareas) )
+                    rl_average_area = np.mean(zareas)
+                    print('    ==> average scale (sqrt[A]) is '+str(round(rl_average_scal,3))+' km')
+                    print('    ==> average side length is '+str(round(rl_average_side,3))+' km')
+                    print('    ==> average area is '+str(round(rl_average_area,1))+' km^2')
+                    del zareas, zsides
+        
+                    if l_force_min_scale:
+                        rdev_old = rdev
+                        #rdev = rl_average_side - rd_spacing
+                        rdev = rl_average_scal - rd_spacing
+                        l_happy = ( abs(rdev) < rtol ) ; # average quadrangle side is close to expected nominal scale
+    
+                        if not l_happy and itt==5:
+                            # We give up after 5 itterations!
+                            print(' +++++ WE GIVE UP !!! ++++++')
+                            l_happy = True
+                        
+                        if not l_happy:
+                            # Linear fit of actual correction as a function of `rd_spacing`
+                            rfc = 0.008*rd_spacing + 0.56                
+                            if itt==1: ralpha = (1.-rfc) / rdev ; # equivalent to a correction of `rfc`
+                            if itt>1 and copysign(1,rdev) == -copysign(1,rdev_old):
+                                ralpha = ralpha/1.3 ; # change of sign of deviation => we decrease alpha!
+                            # will go for a next round with a correction factor becoming increasingly smaller than 1:
+                            #rfcorr = min( max(0.6 , rfcorr - ralpha * rdev ) , 0.95 )
+                            rfcorr = min( max(0.5 , rfcorr - ralpha * rdev ) , 0.95 )
+                            print(' +++++ NEW itteration with: ralpha, rfcorr = ',ralpha, rfcorr)
+                        #
+                    else:
+                        l_happy = True ; # we work with the data's nominal scale so we just go on here...
+                else:
+                    l_happy = True ; # we coud not build any QUAD so we move on anyways...
+                    
+            ###################
+            # #while not l_happy
+    
+            # Save the triangular mesh info:
+            mjt.SaveClassPolygon( cf_npzT, TRIAS, ctype='T' )
+            del TRIAS
+    
+            if l_someQuads:
+                # Save the quadrangular mesh info:
+                mjt.SaveClassPolygon( cf_npzQ, QUADS, ctype='Q' )
+                del QUADS            
+    
+        ### if (not path.exists(cf_npzT)) or (not path.exists(cf_npzQ))
+        ###############################################################
 
-        # Save the triangular mesh info:
-        mjt.SaveClassPolygon( cf_npzT, TRIAS, ctype='T' )
-        del TRIAS
 
-        if l_someQuads:
-            # Save the quadrangular mesh info:
-            mjt.SaveClassPolygon( cf_npzQ, QUADS, ctype='Q' )
-            del QUADS            
+    ### for jf in range(NbF)
+    ########################
 
-    #if (not path.exists(cf_npzT)) or (not path.exists(cf_npzQ))
-    ############################################################
-
+    
+    exit(0)
+        
 
     # Reading the triangle and quad class objects in the npz files:
     TRI = mjt.LoadClassPolygon( cf_npzT, ctype='T' )
