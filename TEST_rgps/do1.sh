8b19afdc428fe4d373c503ee7884ec753c939d5a
brodeau <brodeau@gmail.com>
2023-01-11 12:41:33 +0100

    LB

--- TEST_rgps/do1.sh
+++ TEST_rgps/do1.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-RGPS_DIR="/MEDIA/data/data/RGPS_Kwok_98"
+host=`hostname | cut -d '.' -f2`
+case ${host} in
+    "merlat")
+        RGPS_DIR="/MEDIA/data/data/RGPS_Kwok_98"
+        ;;
+    *)
+        echo "Unsupported host: ${host} !"
+        exit
+esac
+
+
 
 EXE="${HOME}/DEV/mojito/rgps/01_selection_xy.py"
-
 
 
 ${EXE} ${RGPS_DIR}/RGPS_1996-11-07_1997-06-01_traj_LIGHT.nc4 1997
