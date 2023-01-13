e1744e01983cb7323ea85e03bb745bb9bbc2bcf0
brodeau <brodeau@gmail.com>
2023-01-13 16:09:19 +0100

    LB

--- TEST_rgps/conf.bash
+++ TEST_rgps/conf.bash
@@ -15,12 +15,12 @@ case ${host} in
     "merlat")
         RGPS_DIR="/MEDIA/data/data/RGPS_Kwok_98"
         FILIN="RGPS_1996-11-07_1997-06-01_traj_LIGHT.nc4"
-        LIST_STREAM="001"
+        #LIST_STREAM="000 001"
         ;;
     "mcp-oceannext-01")
         RGPS_DIR="/data/gcm_setup/data/RGPS_Kwok_98"
         FILIN="RGPS_1996-11-07_1997-06-01_traj_LIGHT.nc4"
-        LIST_STREAM="001"
+        #LIST_STREAM="000 001"
         ;;
     "frazilo")
         RGPS_DIR="/data/data/RGPS_Kwok_98"
