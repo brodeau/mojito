cf5ba89ad45057e13f8d2d97620585b001f7d2e4
brodeau <brodeau@gmail.com>
2023-01-18 18:11:34 +0100

    LB

--- TEST_rgps/conf.bash
+++ TEST_rgps/conf.bash
@@ -14,26 +14,26 @@ DATE2="${YEAR}0531"
 #LIST_RES="10 20 30"
 LIST_RES="10"
 
+FILIN="RGPS_${YEAR}.nc4"
+
 LIST_STREAM="000 001"
 
 host=`hostname | cut -d '.' -f2`
 case ${host} in
     "merlat")
         export DATA_DIR="/MEDIA/data"
-        FILIN="RGPS_1996-11-07_1997-06-01_traj_LIGHT.nc4"
-        #FILIN="RGPS_1996-11-07_1997-06-01_traj.nc4"
+        FILIN="RGPS_${YEAR}LIGHT.nc4"
         LIST_STREAM="000"
         DATE2="${YEAR}0131"
         ;;
     "mcp-oceannext-01")
         export DATA_DIR="/data/gcm_setup"
-        FILIN="RGPS_1996-11-07_1997-06-01_traj_LIGHT.nc4"
+        FILIN="RGPS_${YEAR}LIGHT.nc4"
         LIST_STREAM="000 001"
         DATE2="${YEAR}0330"
         ;;
     "frazilo")
         export DATA_DIR="/data"
-        FILIN="RGPS_1996-11-07_1997-06-01_traj.nc4"
         DT_BINS_H=$((24*7))  ; # width of a bin for time sampling [hours] 
         #LIST_STREAM="000 001 002 003 004 005"
         LIST_STREAM="000 001"
