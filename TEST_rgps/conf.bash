9c33400b82aa065c5730e3534c9fdf16a047e740
brodeau <brodeau@gmail.com>
2023-01-22 19:54:01 +0100

    LB

--- TEST_rgps/conf.bash
+++ TEST_rgps/conf.bash
@@ -41,7 +41,7 @@ case ${host} in
         #DT_BINS_H=$((12))  ; # width of a bin for time sampling [hours]
         DT_BINS_H=$((48))  ; # width of a bin for time sampling [hours]
         SIZE_MOSAIC=200
-        LIST_STREAM="000 001 002 003"
+        LIST_STREAM="000"
         #DATE1="$((YEAR-1))1230"
         DATE2="${YEAR}0301"
         ;;
