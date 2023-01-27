505febb82297614eff4604e8a3b88acc7a518053
brodeau <brodeau@gmail.com>
2023-01-27 17:32:01 +0100

    LB

--- TEST_rgps/do3.sh
+++ TEST_rgps/do3.sh
@@ -6,6 +6,10 @@
 
 EXE="${MOJITO_DIR}/deformation.py"
 
+mkdir -p logs
+
+ijob=0
+
 for dd in ${LIST_RES}; do
 
     echo; echo
@@ -14,7 +18,7 @@ for dd in ${LIST_RES}; do
 
     for st in ${LIST_STREAM}; do
 
-        list=( `\ls npz/Q-mesh_S${st}_*_${dd}km_*.npz` )
+        list=( `\ls npz/Q-mesh_S${st}*_*_${dd}km_*.npz` )
         nbf=`echo ${list[*]} | wc -w`
         nbf=`expr ${nbf} - 1`
 
@@ -26,22 +30,34 @@ for dd in ${LIST_RES}; do
             jf=0
             while [ ${jf} -lt ${nbf} ]; do
                 echo
-                
+
                 jfp1=`expr ${jf} + 1`
-                
+
                 fQ1=${list[${jf}]}
                 fQ2=${list[${jfp1}]}
-                
+
                 echo " ==> will use:"; echo "     * ${fQ1}"; echo "     * ${fQ2}"
+
+                ijob=$((ijob+1))
+
+                flog=`basename ${fQ1}`
+                flog=`echo ${flog} | sed -e s/".npz"/".out"/g`
                 
                 CMD="${EXE} ${fQ1} ${fQ2} $((DT_BINS_H*3600/2))"
                 echo "  ==> ${CMD}"; echo
-                ${CMD}
+                ${CMD} > ${flog} &
                 echo; echo; echo
-
+                
+                if [ $((ijob%NJPAR)) -eq 0 ]; then
+                    echo "Waiting! (ijob = ${ijob})...."
+                    wait
+                    echo; echo
+                fi
+                
                 jf=`expr ${jf} + 1`
+
             done
-            
+
         fi
 
 
