c9243211b13d98cef475f06a7b08488929de3a75
brodeau <brodeau@gmail.com>
2023-01-31 10:16:39 +0100

    LB

--- TEST_rgps/conf.bash
+++ TEST_rgps/conf.bash
@@ -56,7 +56,7 @@ case ${host} in
         #
         DATE1="${YEAR}0101"
         #DATE2="${YEAR}0115"
-        DATE2="${YEAR}0201"
+        DATE2="${YEAR}0401"
         ;;
     *)
         echo "Unsupported host: ${host} !"
