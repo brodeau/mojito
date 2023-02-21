081f6db9992fcc962be4efd2a41ed708c76032aa
brodeau <brodeau@gmail.com>
2023-02-21 09:32:57 +0100

    LB

---
+++ TEST_brgps/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" = "1" ]; then
+    rm -f figs/SELECTION/*
+
+elif [ "$1" = "2" ]; then
+    rm -f ./figs/quadgener/* npz/[TQ]-mesh_*.npz logs/out_S*__*.out
+
+elif [ "$1" = "3" ]; then
+    rm -f figs/deformation/* npz/DEFORMATIONS_* logs/err_Q-mesh_*.err logs/out_Q-mesh_*.out
+    
+elif [ "$1" = "4" ]; then
+    rm -f npz/PDF_*.npz *PDF*.svg
+    
+elif [ "$1" = "all" ]; then
+    rm -rf figs npz logs *.png *.nc *.svg *.pdf
+
+else
+    echo "Tell me something!"
+
+fi
