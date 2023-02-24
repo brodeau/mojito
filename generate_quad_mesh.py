c0e92e198e2bb272c0a69224cc15243616e2ad5d
brodeau <brodeau@gmail.com>
2023-02-24 12:44:10 +0100

    LB

--- generate_quad_mesh.py
+++ generate_quad_mesh.py
@@ -18,7 +18,7 @@ from climporn import epoch2clock
 import mojito   as mjt
 
 idebug = 0
-iplot  = 1 ; # Create figures to see what we are doing...
+iplot  = 0 ; # Create figures to see what we are doing...
 
 fdist2coast_nc = 'dist2coast/dist2coast_4deg_North.nc'
 
