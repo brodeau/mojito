70907282544486f23b3ce901e3f8c18e9050b399
brodeau <brodeau@gmail.com>
2023-02-28 16:55:54 +0100

    LB

--- TEST_brgps/conf.bash
+++ TEST_brgps/conf.bash
@@ -40,13 +40,13 @@ case ${host} in
         ;;
     "mcp-oceannext-01")
         export DATA_DIR="/data/gcm_setup"
-        FILIN="RGPS_${YEAR}LIGHT.nc4"
-        #FILIN="RGPS_1996-12-21_1997-02-01.nc4"
+        #FILIN="RGPS_${YEAR}LIGHT.nc4"
+        FILIN="RGPS_1996-12-21_1997-02-01.nc4"
         #
         #DT_BINS_H=72  ; # width of a bin for time sampling [hours]
-        DT_BINS_H=6  ; DATE2="${YEAR}0110"
+        #DT_BINS_H=6  ; DATE2="${YEAR}0110"
         #
-        #DATE1="${YEAR}0103_00:00" ; DATE2="${YEAR}0131"; DT_BINS_H=$((24*10))        
+        DATE1="${YEAR}0103_00:00" ; DATE2="${YEAR}0116"; DT_BINS_H=$((24*10))        
         #
         MARKER_SIZE=10
         ;;
