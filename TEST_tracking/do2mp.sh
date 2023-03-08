0cc4d5779fed9b882dc174d6b0a11ae3907cba07
brodeau <brodeau@gmail.com>
2023-03-08 15:34:41 +0100

    LB

--- TEST_tracking/do2mp.sh
+++ TEST_tracking/do2mp.sh
@@ -41,7 +41,7 @@ for ff in ${list_nc}; do
         CMD="${EXE} ${ff} ${lstrec} ${res}"
         echo "    ==> will launch:"; echo "     ${CMD}"; echo
         ${CMD} 1>"./logs/out_${flog}.out" 2>"./logs/err_${flog}.err" &
-        sleep 5
+        sleep 1
         echo
         
         if [ $((ijob%NJPAR)) -eq 0 ]; then
