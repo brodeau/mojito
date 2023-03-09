10e44fe267135bdde489f28da5cc0948f17174c6
brodeau <brodeau@gmail.com>
2023-03-09 12:33:30 +0100

    LB

--- TEST_rgps/do2.sh
+++ TEST_rgps/do2.sh
@@ -4,30 +4,39 @@
 
 EXE="${MOJITO_DIR}/generate_quad_mesh.py"
 
-# Populating nc files we can use:
-list_nc=`\ls nc/SELECTION_RGPS_S???_${YEAR}????h??_${YEAR}????h??.nc`
-nbf=`echo ${list_nc} | wc -w`
-echo " => ${nbf} files => ${nbf} batches!"
 
 
-for ff in ${list_nc}; do
+for res in ${LIST_RES}; do
 
-    fb=`basename ${ff}`
-    echo
-    echo " *** Doing file ${fb}"
-
-    # Number of records inside netCDF file:
-    Nr=`ncdump -h ${ff} | grep 'time\ =\ UNLIMITED' | cut -d'(' -f2 | cut -d' ' -f1`
-    echo "   => it has ${Nr} time records"
-    if [ ${Nr} -ne 2 ]; then
-        echo "  ==> bad! Presently we expect 2 records!"
-        exit
-    fi
+    csf="_${res}km"
+    if [ ${res} -eq 10 ]; then csf=""; fi
 
     
-    for res in ${LIST_RES}; do
 
-    
+    # Populating nc files we can use:
+    list_nc=`\ls nc/SELECTION_RGPS_S???_${YEAR}????h??_${YEAR}????h??${csf}.nc`
+    nbf=`echo ${list_nc} | wc -w`
+    echo " => ${nbf} files => ${nbf} batches!"
+
+
+    for ff in ${list_nc}; do
+
+        fb=`basename ${ff}`
+        echo
+        echo " *** Doing file ${fb}"
+
+        # Number of records inside netCDF file:
+        Nr=`ncdump -h ${ff} | grep 'time\ =\ UNLIMITED' | cut -d'(' -f2 | cut -d' ' -f1`
+        echo "   => it has ${Nr} time records"
+        if [ ${Nr} -ne 2 ]; then
+            echo "  ==> bad! Presently we expect 2 records!"
+            exit
+        fi
+
+
+
+
+
         CMD="${EXE} ${ff} 0,1 ${res}"
         echo "    ==> will launch:"; echo "     ${CMD}"; echo
         ${CMD}
@@ -53,4 +62,3 @@ done
 
 
 #fi
-
