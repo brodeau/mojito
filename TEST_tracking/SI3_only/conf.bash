0db8afeaeda2b87401a32fcd94849a079482116e
brodeau <brodeau@gmail.com>
2023-03-22 18:12:55 +0100

    LB

--- TEST_tracking/SI3_only/conf.bash
+++ TEST_tracking/SI3_only/conf.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-YEAR="1997"
+YEAR="2016"
 
 MOJITO_DIR="${HOME}/DEV/mojito"
 
 NEMO_CONF="NANUK4"
-NEMO_EXP="BBM00"
+NEMO_EXP="BBM2300" ; SBDIR="00000001-00002976_crndg1"
 
 DATE1="${YEAR}0101"
+DATE2="${YEAR}0131"
 
-FSI3IN="${NEMO_CONF}_ICE-${NEMO_EXP}_1h_${YEAR}0101_${YEAR}0331_icemod.nc4"
 
 NJPAR=4 ; # number of jobs we can launch in //
 
@@ -18,19 +18,16 @@ case ${host} in
     "merlat")
         export DATA_DIR="/MEDIA/data"
         #
-        FSI3IN="${NEMO_CONF}_ICE-${NEMO_EXP}_1h_${YEAR}0101_${YEAR}0331_icemod_LIGHT480.nc4"
-        #
         ;;
     "mcp-oceannext-01")
         export DATA_DIR="/data/gcm_setup"
         #
         NJPAR=4       
         #
-        #FSI3IN="${NEMO_CONF}_ICE-${NEMO_EXP}_1h_${YEAR}0101_${YEAR}0331_icemod_LIGHT480.nc4"
-        #
         ;;
     "frazilo")
-        export DATA_DIR="/data"
+        #export DATA_DIR="/data"
+        export DATA_DIR="/home/data/laurent/tmp"
         #
         NJPAR=30
         #
@@ -46,7 +43,11 @@ esac
 DIRIN_PREPARED_RGPS="${MOJITO_DIR}/TEST_rgps/nc"
 
 
-export FSI3IN="${DATA_DIR}/${NEMO_CONF}/${NEMO_EXP}/${FSI3IN}"
+FSI3IN="${NEMO_CONF}_ICE-${NEMO_EXP}_1h_${DATE1}_${DATE2}_icemod.nc4"
+
+export FSI3IN="${DATA_DIR}/${NEMO_CONF}/${NEMO_CONF}_ICE-${NEMO_EXP}-S/${SBDIR}/${FSI3IN}"
+
+#export FSI3IN="${DATA_DIR}/${NEMO_CONF}/${NEMO_EXP}/${FSI3IN}"
 
 export FNMM="${DATA_DIR}/${NEMO_CONF}/${NEMO_CONF}.L31-I/mesh_mask_${NEMO_CONF}_L31_4.2_1stLev.nc"
 
