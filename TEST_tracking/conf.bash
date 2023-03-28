ece266fc0a488ab1354150578c9a7b137220b897
brodeau <brodeau@gmail.com>
2023-03-28 13:27:57 +0200

    LB

--- TEST_tracking/conf.bash
+++ TEST_tracking/conf.bash
@@ -5,8 +5,8 @@ YEAR="1997"
 MOJITO_DIR="${HOME}/DEV/mojito"
 
 NEMO_CONF="NANUK4"
-#NEMO_EXP="BBM00"
-NEMO_EXP="EVP00"
+NEMO_EXP="BBM00"
+#NEMO_EXP="EVP00"
 
 DATE1="${YEAR}0101"
 
@@ -43,19 +43,11 @@ case ${host} in
         exit
 esac
 
-
 DIRIN_PREPARED_RGPS="${MOJITO_DIR}/TEST_rgps/nc"
-
-#export FNCSEED="${MOJITO_DIR}/TEST_rgps/${FNCSEED}"
-##export FNCSEED="${DATA_DIR}/data/mojito/seeding_from_rgps/${FNCSEED}"
-
-#export FNCSEED="/data/data/mojito/seeding_from_rgps/RGPS_ice_drift_1997-01-01_1997-05-01_lb.nc"
-export FNCSEED="/data/data/mojito/seeding_from_rgps/RGPS_tracking_1997-01-01_1997-02-05_NEW.nc"
 
 export FSI3IN="${DATA_DIR}/${NEMO_CONF}/${NEMO_EXP}/${FSI3IN}"
 
 export FNMM="${DATA_DIR}/${NEMO_CONF}/${NEMO_CONF}.L31-I/mesh_mask_${NEMO_CONF}_L31_4.2_1stLev.nc"
 
 mkdir -p ./figs ./npz
-
 
