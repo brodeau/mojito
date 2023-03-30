0e5f6232cc699faa7714fc5aecf0fbb60eb32c3d
brodeau <brodeau@gmail.com>
2023-03-30 12:09:07 +0200

    LB

--- TEST_rgps/do1.sh
+++ TEST_rgps/do1.sh
@@ -13,7 +13,11 @@ YEAR=`echo ${DATE1} | cut -c1-4`
 MMDD1=`echo ${DATE1} | cut -c5-8`
 MMDD2=`echo ${DATE2} | cut -c5-8`
 
-CMD="${EXE} ${RGPS_DIR}/${FILIN} ${DATE1} ${DATE2} ${DT_BINS_H}"
+if [ ${RESKM} -ge 50 ] && [ ${DT_BINS_H} -ge 72 ]; then
+    CMD="${EXE} ${RGPS_DIR}/${FILIN} ${DATE1} ${DATE2} ${DT_BINS_H} $((DT_BINS_H*2/3))"
+else
+    CMD="${EXE} ${RGPS_DIR}/${FILIN} ${DATE1} ${DATE2} ${DT_BINS_H}"
+fi
 
 echo
 echo " *** About to launch:"; echo "     ${CMD}"; echo
