abf8abd9b879a080a223b44375162b600f36b346
brodeau <brodeau@gmail.com>
2023-04-04 15:42:26 +0200

    LB

--- TEST_rgps/do5mkPDFs.sh
+++ TEST_rgps/do5mkPDFs.sh
@@ -2,7 +2,7 @@
 
 . ./conf.bash
 
-EXE="${MOJITO_DIR}/statistics.py"
+EXE="${MOJITO_DIR}/mkpdfs.py"
 
 
 
@@ -11,8 +11,14 @@ fdiv=`\ls npz/def_DIV_RGPS_dt${DT_BINS_H}_${RESKM}km_${YEAR}????-${YEAR}????.npz
 
 nbh=`echo ${fdiv} | wc -w`
 if [ ${nbh} -ne 1 ]; then
-    echo "PROBLEM: more than 1 file found !!!"; exit
+    if [ ${nbh} -eq 0 ]; then
+        echo "ERROR: no divergence file found!!!"
+    else
+        echo "PROBLEM: more or less than 1 file found !!!"
+    fi
+    exit
 fi
+
 
 CMD="${EXE} ${fdiv}"
 echo "  ==> ${CMD}"; echo
