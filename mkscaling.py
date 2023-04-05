420f9339d1675c6a383f435d5ff260ab0ffcef0a
brodeau <brodeau@gmail.com>
2023-04-05 12:26:52 +0200

    LB

--- mkscaling.py
+++ mkscaling.py
@@ -19,7 +19,7 @@ l_cst_bins = False ; rfexp_bin = 0.2
 cprefixIn='DEFORMATIONS_' ; # Prefix of deformation files...
 
 #do_scales = np.array([ 10, 20, 40, 320, 640 ], dtype=int)
-do_scales = np.array([ 10, 20, 320, 640 ], dtype=int)
+do_scales = np.array([ 10, 20, 40, 320, 640 ], dtype=int)
 
 cfield = 'total'; cfld = 'tot'; cFLD = 'TOT'
 
@@ -99,6 +99,7 @@ if __name__ == '__main__':
 
 
     exit(0)
+    
 
 
     #
