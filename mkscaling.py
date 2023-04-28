0da1f2117b44348e6e6317efd53f8298a9f7be2a
brodeau <brodeau@gmail.com>
2023-04-28 11:17:27 +0200

    LB

--- mkscaling.py
+++ mkscaling.py
@@ -25,21 +25,24 @@ cfield = 'total'; cfld = 'tot'; cFLD = 'TOT'
 #cfield = 'shear'; cfld = 'shr'; cFLD = 'SHR'
 #cfield = 'divergence'; cfld = 'div'; cFLD = 'DIV'
 
-vORIGS = ['RGPS','BBM','EVP']
-
 lOnlyRGPS = False
 
 if __name__ == '__main__':
 
     kk = cfg.initialize( mode='model' )
     
-    if not len(argv)==3:
-        print('Usage: '+argv[0]+' <dir_npz_in> <list_scales>')
+    if not len(argv)==4:
+        print('Usage: '+argv[0]+' <dir_npz_in> <list_exp> <list_scales>')
         exit(0)
     dir_npz_in = argv[1]
-    lst_scales = argv[2]
-    do_scales = np.array( split(',',lst_scales), dtype=int )
+    lst_exp    = argv[2]
+    lst_scales = argv[3]
     
+    do_scales = np.array( split(',',lst_scales), dtype=int  )
+    vORIGS    = np.array( split(',',lst_exp),    dtype='U16')
+
+
+
     #dir_npz_in = str.replace( dir_npz_in, '<HOME>', environ.get('HOME') )
 
     print('\n *** Will find deformation files into: '+dir_npz_in)
@@ -47,12 +50,13 @@ if __name__ == '__main__':
     print('\n *** Scales to work with:', do_scales)
 
     Nscl = len(do_scales)
+    Norg = len(vORIGS)
 
-    cf  = np.zeros((Nscl,3), dtype='U256' )
-    Nbp = np.zeros((Nscl,3), dtype=int    )
-    xMQ = np.zeros((Nscl,3,3)) -9999. ; # Moments: [scale,origin,order]
-    xAq = np.zeros((Nscl,3)  ) -9999. ; # Mean quad area: [scale,origin]
-    reskm_actual = np.zeros((Nscl,3)  ) -9999. ; # actual resolution we're dealing with
+    cf  = np.zeros((Nscl,Norg), dtype='U256' )
+    Nbp = np.zeros((Nscl,Norg), dtype=int    )
+    xMQ = np.zeros((Nscl,Norg,3)) -9999. ; # Moments: [scale,origin,order]
+    xAq = np.zeros((Nscl,Norg)  ) -9999. ; # Mean quad area: [scale,origin]
+    reskm_actual = np.zeros((Nscl,Norg)  ) -9999. ; # actual resolution we're dealing with
 
 
 
@@ -123,8 +127,8 @@ if __name__ == '__main__':
     Nl_max = np.max(Nbp)
     print(' *** Biggest sample: ',str(Nl_max),'points!')
 
-    xXQ = np.zeros((Nl_max,Nscl,3,3)) -9999. ; # Moments: [points,scale,origin,order]
-    xXS = np.zeros((Nl_max,Nscl,3)) -9999. ; # Moments: [points,scale,origin]
+    xXQ = np.zeros((Nl_max,Nscl,Norg,3)) -9999. ; # Moments: [points,scale,origin,order]
+    xXS = np.zeros((Nl_max,Nscl,Norg)) -9999. ; # Moments: [points,scale,origin]
 
     
     
