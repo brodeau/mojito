aa8b79f41fea3e0b6620753951c9531061a5e1f0
brodeau <brodeau@gmail.com>
2023-05-23 17:45:03 +0200

    LB

--- diags/mk90p.py
+++ diags/mk90p.py
@@ -21,7 +21,72 @@ Nmin = 1000 ; # smallest `N` (size of the sample at a given date) required to ac
 
 
 
-def PlotP90Series( vt, VS, figname='fig_series_P90.png', y_range=(0.,0.1), x_range=None, dy=0.01 ):
+
+
+def Construct90P( ifile, vdates_batch, pdates, pdef ):
+    '''
+        * ifile: number of the file treated
+        
+    '''
+    lmaskArrays = True
+    (nbF,) = np.shape(vdates_batch)
+    
+    z90p = np.zeros( nbF )
+    zdat = np.zeros( nbF, dtype=int )
+    imsk = np.zeros( nbF, dtype='i1' )
+    
+    ic = 0
+    for jd in vdates_batch:
+        print('\n *** file #'+str(ifile)+''+e2c(jd))
+
+        (idxDate,) = np.where( pdates == jd )
+        nV = len(idxDate)
+        print('         => '+str(nV)+' '+cv_in+' deformation for this date....')
+        
+        ztmp = pdef[idxDate]
+        xdef = np.sort(ztmp)
+
+        if xdef.shape != (nV,):
+            print('ERROR [Construct90P()]: problem #1'); exit(0)
+
+        ri90 = 0.9*float(nV)
+        i90 = int( floor( ri90 ) )
+        rw = ri90 - float(i90)
+
+        if nV>=Nmin and i90<nV-2:
+            # sample size must be large enough
+            # and: otherwize we are too close to the end of the series, this probably a bad batch!
+            z90 = (1.-rw)*xdef[i90] + rw*xdef[i90+1]
+            
+            z90p[ic] = z90
+            zdat[ic] =  jd
+            imsk[ic] = 1
+            if z90>=Nmin:
+                imsk[ic] = 1            
+            #print('         => z90 =', z90, '(mask =',imsk[ic],')')
+            
+        ic+=1
+
+    if lmaskArrays:
+        z90pm = np.ma.masked_where(imsk!=1, z90p)
+        zdatm = np.ma.masked_where(imsk!=1, zdat)
+    
+        zt = np.ma.MaskedArray.compressed( zdatm )
+        zx = zt[1:] - zt[:-1]
+        if np.any(zx<=0):
+            print('ERROR [Construct90P]: `zdatm` is not increasing!'); exit(0)
+        #print(zx)    
+        print('')        
+        return zt, np.ma.MaskedArray.compressed( z90pm )
+    else:
+        (idxM,) = np.where(imsk==0)
+        z90p[idxM] = 0.
+
+        return zdat, z90p
+
+    
+
+def PlotP90Series( vt, V1, V2=[], V3=[], figname='fig_series_P90.png', vlabels=[], y_range=(0.,0.1), x_range=None, dy=0.01 ):
     '''
         * vt: time array as Epoch Unix time 
     '''
@@ -30,27 +95,29 @@ def PlotP90Series( vt, VS, figname='fig_series_P90.png', y_range=(0.,0.1), x_ran
     import matplotlib.pyplot as plt
     import matplotlib.dates as mdates
     from datetime import datetime as dt
-    #ras = np.mean(VS) ; ram = np.mean(VM)
-    #ymin, ymax, dy = sym_round_bounds(min(np.min(VM-ram),np.min(VS-ras)), max(np.max(VM-ram), np.max(VS-ras)), base=0.1 )
+    #ras = np.mean(V1) ; ram = np.mean(VM)
+    #ymin, ymax, dy = sym_round_bounds(min(np.min(VM-ram),np.min(V1-ras)), max(np.max(VM-ram), np.max(V1-ras)), base=0.1 )
 
 
+    (nT,) = np.shape(vt)
+
+    if np.shape(V1) != (nT,):
+        print('ERROR: `V1` and `vt` do not agree in shape!'); exit(0)
+    
+    ldo2 = ( np.shape(V2) == (nT,) )
+    ldo3 = ( np.shape(V2) == (nT,) )
+
+    if ldo3 and not ldo2:
+        print('ERROR: `ldo3 and not ldo2` !!!'); exit(0)
+    
     if x_range:
         (xmin,xmax) = x_range
     else:
         (xmin,xmax) = (np.min(vt), np.max(vt))
 
+    
 
-    (nT,) = np.shape(vt)
-    vdate = np.zeros(nT, dtype='U10')
-
-    for jt in range(nT):
-        #cdate = 
-        #print(cdate)
-        vdate[jt] = e2c(vt[jt], precision='D')
-        #vdate = e2c(vt[jt])
-        #print(vdate[jt])
-    #exit(0)
-
+    vdate = np.array([ e2c(vt[jt], precision='D') for jt in range(nT) ], dtype='U10')
     
     VX = [dt.strptime(d, "%Y-%m-%d").date() for d in vdate]
 
@@ -64,8 +131,8 @@ def PlotP90Series( vt, VS, figname='fig_series_P90.png', y_range=(0.,0.1), x_ran
     plt.xticks(rotation='60')
 
     plt.plot(VX, vt*0.0         , '-', color='k',                     label=None,  zorder=5)
-    plt.plot(VX, VS, 'o', color='r', markersize=6, alpha=0.5, label='RGPS', zorder=10)
-    #plt.plot(VX, VM-ram, '.', color=clr_mod, markersize=6, alpha=0.5, label=clabM, zorder=15)
+    plt.plot(VX, V1, 'o', color='r', markersize=6, alpha=0.5, label='RGPS', zorder=10)
+
 
     (ymin,ymax) = y_range
 
@@ -90,14 +157,18 @@ def PlotP90Series( vt, VS, figname='fig_series_P90.png', y_range=(0.,0.1), x_ran
 
 if __name__ == '__main__':
 
-    if not len(argv) in [2]:
-        print('Usage: '+argv[0]+' <file_deformation_gatheres.npz>')
+    Narg = len(argv)
+    if not Narg in [2,3,4]:
+        print('Usage: '+argv[0]+' <file_deformation_gatherd.npz> (<file_deformation_gatherd.npz>) (<file_deformation_gatherd.npz>)')
         exit(0)
 
-    cf_in = argv[1]
+    cf_in1 = argv[1]
+
+    ldo2 = (Narg==3)
+    ldo3 = (Narg==4)
 
     # What fields are we dealing with based on filename:
-    cv_in = split( '_', path.basename(cf_in) )[1]
+    cv_in = split( '_', path.basename(cf_in1) )[1]
     print(cv_in)
 
     if cv_in == 'DIV':
@@ -113,114 +184,59 @@ if __name__ == '__main__':
         print('ERROR: wrong `cv_in` !!! ', cv_in); exit(0)
 
     
-    cd_in = path.dirname(cf_in)
+    #cd_in = path.dirname(cf_in1)
     
-    with np.load(cf_in) as data:
-        dtbin = data['dtbin']
-        reskm = data['reskm_nmnl']
-        corigin = str(data['origin'])        
-        cperiod = str(data['period'])
-        nbF = int(data['Nbatch'])
-        vdates_batch = data['dates_batch']
-        Zdat = data['dates_point']
-        ZDEF = data[cvar]
-        
+    with np.load(cf_in1) as data:
+        dtbin1 = data['dtbin']
+        reskm1 = data['reskm_nmnl']
+        corigin1 = str(data['origin'])        
+        #cperiod1 = str(data['period'])
+        nbF1 = int(data['Nbatch'])
+        VDTB1 = data['dates_batch']
+        Zdat1 = data['dates_point']
+        ZDEF1 = data[cvar]
+    cdtbin1 = str(dtbin1)
+    if np.shape(VDTB1) != (nbF1,):
+        print('ERROR: problem #0 for file 1!'); exit(0)
+                
+    if ldo2:
+        with np.load(cf_in2) as data:
+            dtbin2 = data['dtbin']
+            reskm2 = data['reskm_nmnl']
+            corigin2 = str(data['origin'])        
+            #cperiod2 = str(data['period'])
+            nbF2 = int(data['Nbatch'])
+            VDTB2 = data['dates_batch']
+            Zdat2 = data['dates_point']
+            ZDEF2 = data[cvar]
+            if reskm2!=reskm1:
+                print('ERROR: wrong resolutin for '+cf_in2+'!!! ', reskm2); exit(0)
+        cdtbin2 = str(dtbin2)
+        if np.shape(VDTB2) != (nbF2,):
+            print('ERROR: problem #0 for file 2!'); exit(0)
 
-    cdtbin = str(dtbin)
-    reskm  = reskm
+    if ldo3:
+        with np.load(cf_in3) as data:
+            dtbin3 = data['dtbin']
+            reskm3 = data['reskm_nmnl']
+            corigin3 = str(data['origin'])        
+            #cperiod3 = str(data['period'])
+            nbF3 = int(data['Nbatch'])
+            VDTB3 = data['dates_batch']
+            Zdat3 = data['dates_point']
+            ZDEF3 = data[cvar]
+            if reskm3!=reskm1:
+                print('ERROR: wrong resolutin for '+cf_in3+'!!! ', reskm3); exit(0)
+        cdtbin3 = str(dtbin3)
+        if np.shape(VDTB3) != (nbF3,):
+            print('ERROR: problem #0 for file 3!'); exit(0)
+
+        
+    reskm  = reskm1
     creskm = str(reskm)
-
-
-
-    if np.shape(vdates_batch) != (nbF,):
-        print('ERROR: problem #0!'); exit(0)
-
-
-    VF90 = np.zeros( nbF )
-    imsk = np.zeros( nbF, dtype='i1' )
     
-    # For large scales, we must increase the size of bins:
-    #zmin_div, zmin_shr, zmin_tot = 0.003, 0.003, 0.003 ; # day^-1 ; RGPS is noisy around 0! We do not want have the zero on the PDF...
-    #if   reskm >= 15. and reskm < 35.:
-    #    zmin_div, zmin_shr, zmin_tot = 0.001, 0.001, 0.001
-    #elif   reskm >= 35. and reskm < 50.:
-    #    zmin_div, zmin_shr, zmin_tot = 5.e-4, 5.e-4, 5.e-4
-    #    wVbin_min = 1.5*wVbin_min        
-    #    rfexp_bin = rfexp_bin*1.2
-    #elif reskm >= 50. and reskm < 100.:
-    #    zmin_div, zmin_shr, zmin_tot = 1.e-4, 1.e-4, 1.e-4
-    #    wVbin_min = 2.*wVbin_min
-    #    rfexp_bin = rfexp_bin*1.5
-    #elif reskm >= 100. and reskm < 200.:
-    #    zmin_div, zmin_shr, zmin_tot = 1.e-5, 1.e-5, 1.e-5
-    #    wVbin_min = 4.*wVbin_min
-    #    rfexp_bin = rfexp_bin*2.
-    #elif reskm >= 200. and reskm < 400.:
-    #    zmin_div, zmin_shr, zmin_tot = 1.e-5, 1.e-5, 1.e-5
-    #    wVbin_min = 6.*wVbin_min
-    #    rfexp_bin = rfexp_bin*2.5
-    #elif reskm >= 400. and reskm < 700.:
-    #    zmin_div, zmin_shr, zmin_tot = 1.e-5, 1.e-5, 1.e-5
-    #    wVbin_min = 8.*wVbin_min
-    #    rfexp_bin = rfexp_bin*2.5
-    #if reskm >= 35.:
-    #    print('\n * Increased the width of bins and exp growth!!! Because large scale! wVbin_min, rfexp_bin =',wVbin_min, rfexp_bin)
-            
-    #print(ZDEF)
+
+    VDAT1, V90P1 = Construct90P(1, VDTB1, Zdat1, ZDEF1  )
 
 
-    print('\n *** All availabled dates for deformation:')
-
-    ic = 0
-    for jd in vdates_batch:
-        print('\n *** '+e2c(jd))
-
-        (idxDate,) = np.where( Zdat == jd )
-        nV = len(idxDate)
-        print('         => '+str(nV)+' '+cv_in+' deformation for this date....')
-        
-        ztmp = ZDEF[idxDate]
-        #xdef = np.sort(ztmp[::-1])
-        xdef = np.sort(ztmp)
-
-        if xdef.shape != (nV,):
-            print('ERROR: problem #1'); exit(0)
-                        
-        #for zd in xdef:
-        #    print(zd)
-
-
-        ri90 = 0.9*float(nV)
-        #print(" ri90 = ", ri90, '/', nV)
-        i90 = int( floor( ri90 ) )
-        #print(" i90 = ", i90, '/', nV)
-        rw = ri90 - float(i90)
-        #print(" rw = ", rw)
-
-        #print('lolo: nV, Nmin, i90, nV-1 ')
-        if nV>=Nmin and i90<nV-2:
-            # sample size must be large enough
-            # and: otherwize we are too close to the end of the series, this probably a bad batch!
-            v90 = (1.-rw)*xdef[i90] + rw*xdef[i90+1]
-            #print(" v90 =",v90, " v90_floor=",xdef[i90], " v90_ceil=",xdef[i90+1])
-            
-            VF90[ic] = v90
-            imsk[ic] = 1
-            if v90>=Nmin:
-                imsk[ic] = 1
-
-            
-        print('         => v90 =', v90, '(mask =',imsk[ic],')')
-            
-        ic+=1
-
-
-        
-    print('')
-
-
-    V90 = np.ma.masked_where(imsk!=1, VF90)
-
-
-
-    kk= PlotP90Series( vdates_batch, V90, figname='./figs/series.png', y_range=(0.,0.1), dy=0.01 )
+    kk= PlotP90Series( VDAT1, V90P1, figname='./figs/series.png', y_range=(0.,0.1), dy=0.01 )
