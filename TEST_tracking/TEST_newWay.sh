1bc281251e0eb1efbf66071d41bfef28c6a03a83
brodeau <brodeau@gmail.com>
2023-05-25 16:27:38 +0200

    LB

--- TEST_tracking/TEST_newWay.sh
+++ TEST_tracking/TEST_newWay.sh
@@ -5,11 +5,13 @@ EXE2="python3 -u /home/laurent/DEV/sitrack/si3_part_tracker.py"
 
 rm -f *.png
 
-STR="S004_dt72_19970115-08h44t0"
+STR="S004_dt72_19970115"
 
-${EXE1} ../TEST_rgps/npz//Q-mesh_RGPS_${STR}
+${EXE1} ../TEST_rgps/npz/Q-mesh_RGPS_${STR}
+
+fout=`\ls ./nc/Q-mesh_RGPS_${STR}h??_*km.nc`
 
 
 ${EXE2} -i /MEDIA/data/NANUK4/BBM00/NANUK4_ICE-BBM00_1h_19970101_19970331_icemod_LIGHT480.nc4 \
        -m /MEDIA/data/NANUK4/NANUK4.L31-I/mesh_mask_NANUK4_L31_4.2_1stLev.nc \
-       -s  ~/DEV/mojito/TEST_rgps/nc/Q-mesh_RGPS_${STR}.nc
+       -s  ${fout}
